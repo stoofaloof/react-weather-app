52ab54463c87edd00bcb954a8cafe932ee2a21e6
Steph C <stephanie.wy.chow@gmail.com>
2021-08-07 23:04:35 -0400

    initial css set up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
+import Weather from "./Weather.js";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
-        This project was coded by Stephanie Chow and is {""}
-        <a
-          className="Github-link"
-          href="https://github.com/stoofaloof/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
+        <Weather />
+        <footer>
+          {" "}
+          This project was coded by Stephanie Chow and is {""}
+          <a
+            className="Github-link"
+            href="https://github.com/stoofaloof/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
       </div>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3 d-grid gap-2 d-md-flex justify-content-md-end">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary me-md-1"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>Toronto, Canada</h1>
+      <ul className="TopHeader">
+        <li>Saturday 10:27 PM</li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+            alt="cloudy"
+          />
+          <div className="temperature">22°C</div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 88%</li>
+            <li>Wind: 10 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
