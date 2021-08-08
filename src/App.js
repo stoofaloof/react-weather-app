a15a027fcdb4f5a1f06dfc97ba9760cf95f29c2c
Steph C <stephanie.wy.chow@gmail.com>
2021-08-07 22:23:07 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <p>This project was coded by Stephanie Chow and is</p>
-      <a
-        className="Github-link"
-        href="https://github.com/stoofaloof/react-weather-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        open-sourced on GitHub
-      </a>
+      <div className="container">
+        <h1>Weather App</h1>
+        This project was coded by Stephanie Chow and is {""}
+        <a
+          className="Github-link"
+          href="https://github.com/stoofaloof/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </div>
     </div>
   );
 }
